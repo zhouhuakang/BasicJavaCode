43fb7556da2abd5eeda77b6bf3f5a4da61bdca39
hank <459673409@qq.com>
2017-04-15 11:35:42 +0800

    no message

--- src/hank/search/BinarySearch.java
+++ src/hank/search/BinarySearch.java
@@ -19,12 +19,13 @@ public class BinarySearch {
 		}
 		return 0;
 	}
+	
 
-	// public static void main(String[] args) {
-	// int[] a = { -1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-	// System.out.println(new BinarySearch().binarySearch(a, 33));
-	// System.out.println(new BinarySearch().binarySearch(a, 1));
-	// System.out.println(new BinarySearch().binarySearch(a, 10));
-	// }
+	 public static void main(String[] args) {
+	 int[] a = { -1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	 System.out.println(new BinarySearch().binarySearch(a, 33));
+	 System.out.println(new BinarySearch().binarySearch(a, 1));
+	 System.out.println(new BinarySearch().binarySearch(a, 10));
+	 }
 
 }
