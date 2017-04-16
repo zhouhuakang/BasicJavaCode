c7d78f075856e7d79d56a0f0c55b1da57496f191
hank <459673409@qq.com>
2017-04-16 11:06:46 +0800

    实现快速排序

--- src/hank/sort/QuickSort.java
+++ src/hank/sort/QuickSort.java
@@ -1,6 +1,39 @@
 package hank.sort;
 
+import java.util.Arrays;
+
 public class QuickSort {
 
-	
+	public void sort(int a[], int low, int high) {
+		if (low > high) {
+			return;
+		}
+		int index = a[low];
+		int i = low;
+		int j = high;
+		while (i < j) {
+			while (i < j && a[j] >= index) {
+				j--;
+			}
+			if (i < j) {
+				a[i++] = a[j];
+			}
+			while (i < j && a[i] < index) {
+				i++;
+			}
+			if (i < j) {
+				a[j--] = a[i];
+			}
+		}
+		a[i] = index;// i现在所指的位置是中间位置
+		sort(a, low, i - 1);
+		sort(a, i + 1, high);
+	}
+
+	public static void main(String[] args) {
+		int a[] = { 9, 20, 3, 5, 6, 8, 22, 11 };
+		int high = a.length - 1;
+		new QuickSort().sort(a, 0, high);
+		System.out.println(Arrays.toString(a));
+	}
 }
